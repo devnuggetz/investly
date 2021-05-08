4654b2bce346bfca2f7431c9194d4d351b073af5
shuvamk <kshuvam564@gmail.com>
2021-05-08 23:08:00 +0530

    Designed Example Screen

--- components/example/index.js
+++ components/example/index.js
@@ -7,11 +7,12 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
+import HTML from 'react-native-render-html';
 import {connect} from 'react-redux';
 import {icons} from '../../constants';
 import Safeview from '../common/safeview';
 
-const Example = ({navigation}) => {
+const Example = ({navigation, example}) => {
   return (
     <Safeview style={styles.container}>
       <View style={styles.top}>
@@ -22,34 +23,51 @@ const Example = ({navigation}) => {
             resizeMode="contain"
           />
         </TouchableOpacity>
-        <Text>Example</Text>
+        <Text style={styles.example}>Example</Text>
       </View>
-      <ScrollView></ScrollView>
+      <ScrollView showsVerticalScrollIndicator={false} style={styles.content}>
+        <Image
+          source={{uri: example.exampleImage}}
+          style={styles.banner}
+          resizeMode="contain"
+        />
+        <HTML source={{html: example.exampleContent}} />
+      </ScrollView>
     </Safeview>
   );
 };
 
 const mapStateToProps = ({module}) => {
-  console.log(module.currentExample);
-  return {};
+  return {
+    example: module.currentExample,
+  };
 };
 
 export default connect(mapStateToProps)(Example);
 
 const styles = StyleSheet.create({
+  banner: {
+    height: 200,
+    marginVertical: 12,
+  },
   close: {
     height: 30,
+    width: 30,
     tintColor: 'red',
-    position: 'absolute',
-    left: -40,
   },
   container: {
     paddingVertical: 16,
   },
+  content: {
+    paddingHorizontal: 16,
+  },
+  example: {
+    fontSize: 18,
+    fontWeight: '600',
+  },
   top: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    position: 'relative',
-    paddingRight: 12,
+    padding: 16,
   },
 });
